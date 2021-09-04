410b8d87f10fa1c712870b4131ec91fe4431dc21
rka <russkikh_k@ekb.sima-land.ru>
2021-09-04 23:57:35 +0500

    done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,20 @@
 let menuLink = document.getElementsByClassName('menu__link')
 let currentActiveElement = null
 
-const onClick = function (e) {
+for (let i = 0; i < menuLink.length; i++) {
+    menuLink[i].onclick = onClick
+}
+
+function onClick(e) {
     if (currentActiveElement != null) {
         currentActiveElement.classList.remove('menu_active')
     }
-    let element = e.target
-    if (!element.hasAttribute('href') || element.getAttribute('href') != '') {
+
+    if (!this.hasAttribute('href') || this.getAttribute('href') != '') {
         return
     }
-    currentActiveElement = element.parentElement.querySelector('.menu .menu_sub')
+    currentActiveElement = this.parentElement.querySelector('.menu .menu_sub')
     currentActiveElement.classList.add('menu_active')
     return false;
-}
-
-for (let i = 0; i < menuLink.length; i++) {
-    menuLink[i].onclick = onClick
 }
 
